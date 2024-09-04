e822fb374227623f22088024ea3de8be4004c9f5
vhj1995 <vhj1995@gmail.com>
2024-09-05 01:16:07 +0530

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TempClean__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(12, 78);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(106, 29);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(124, 78);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(113, 29);
+            this.btnClean.TabIndex = 1;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press Scan to Continue";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(298, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(322, 122);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnScan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TempClean ++";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TempClean__
+{
+    public partial class Form1 : Form
+    {
+        public static string tmpPath = String.Empty;
+        public static List<string> files = new List<string>();
+        public static DirectoryInfo folder;
+        public static DirectoryInfo folder2;
+        public static DirectoryInfo folder3;
+        public Form1()
+        {
+            InitializeComponent();
+            Scan();
+        }
+        static long folderSize(DirectoryInfo folder)
+        {
+            long totalSizeOfDir = 0;
+
+            // Get all files into the directory
+            FileInfo[] allFiles = folder.GetFiles();
+
+            // Loop through every file and get size of it
+            foreach (FileInfo file in allFiles)
+            {
+                totalSizeOfDir += file.Length;
+            }
+
+            // Find all subdirectories
+            DirectoryInfo[] subFolders = folder.GetDirectories();
+
+            // Loop through every subdirectory and get size of each
+            foreach (DirectoryInfo dir in subFolders)
+            {
+                totalSizeOfDir += folderSize(dir);
+            }
+
+            // Return the total size of folder
+            return totalSizeOfDir;
+        }
+        private void ThreadSafe(MethodInvoker method)
+        {
+            try
+            {
+                if (InvokeRequired)
+                    Invoke(method);
+                else
+                    method();
+            }
+            catch (ObjectDisposedException) { }
+        }
+        public void Scan()
+        {
+            tmpPath = Path.GetTempPath();
+            files.Clear();
+            files.AddRange(Directory.GetFiles(tmpPath, "*.*", SearchOption.AllDirectories));
+            files.AddRange(Directory.GetFiles("C:\\Windows\\Temp", " *.*", SearchOption.AllDirectories));
+            //files.AddRange(Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)+ "\\Temp","*.*", SearchOption.AllDirectories));
+
+            folder = new DirectoryInfo(tmpPath);
+            folder2 = new DirectoryInfo("C:\\Windows\\Temp");
+            //folder3 = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Temp");
+
+            // Calling a folderSize() method
+            long totalFolderSize = folderSize(folder);
+            totalFolderSize += folderSize(folder2);
+            //totalFolderSize += folderSize(folder3);
+
+            label1.Text = "Total Size of Files to Clean : " + FileSizeFormatter.FormatSize(totalFolderSize);
+        }
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            Scan();
+        }
+        public static class FileSizeFormatter
+        {
+            // Load all suffixes in an array  
+            static readonly string[] suffixes =
+            { " Bytes", " KB", " MB", " GB", " TB", " PB" };
+            public static string FormatSize(Int64 bytes)
+            {
+                int counter = 0;
+                decimal number = (decimal)bytes;
+                while (Math.Round(number / 1024) >= 1)
+                {
+                    number = number / 1024;
+                    counter++;
+                }
+                return string.Format("{0:n1}{1}", number, suffixes[counter]);
+            }
+        }
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            progressBar1.Show();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                int calcPercentage = 0;
+                for (int i = 0; i < files.Count(); i++)
+                {
+                    calcPercentage = (100 * i) / files.Count();
+                    if (File.Exists(files[i]))
+                    {
+                        try
+                        {
+                            File.Delete(files[i]);
+                            ThreadSafe(() => label1.Text = "Deleting... " + Path.GetFileName(files[i]));
+                        }
+                        catch { }
+
+                        backgroundWorker1.ReportProgress(calcPercentage);
+                    }
+                }
+
+                //Delete Folders
+                foreach (DirectoryInfo dir in folder.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                foreach (DirectoryInfo dir in folder2.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                foreach (DirectoryInfo dir in folder3.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch {
+                //backgroundWorker1.ReportProgress(calcPercentage);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Hide();
+            //MessageBox.Show("Done");
+            label1.Text = "Done!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Hide();
+        }
+    }
+}
